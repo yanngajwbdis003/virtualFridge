83d7a8e812ecc87192d65cdf8c1c8815f4a8869c
mo <yanngajwbdis@gmail.com>
2024-05-18 12:38:19 +0800

    add firebase dependency + rough set up for login

---
+++ virtualFridge/Model/FoodItem.swift
@@ -0,0 +1,8 @@
+//
+//  FoodItem.swift
+//  virtualFridge
+//
+//  Created by Monica Yang on 2024-05-16.
+//
+
+import Foundation
---
+++ virtualFridge/View/TLButton.swift
@@ -0,0 +1,8 @@
+//
+//  TLButton.swift
+//  virtualFridge
+//
+//  Created by Monica Yang on 2024-05-18.
+//
+
+import Foundation
